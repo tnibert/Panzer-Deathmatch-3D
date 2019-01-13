b780334a0d6cefad5eeaaffb8ec596f48c99c06c
Tim Nibert <docz2a@gmail.com>
2019-01-13 17:17:54 +1100

    we have a build!

---
+++ build.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+/home/tim/code/godot/godot/bin/godot.x11.opt.tools.64.mono --export "Linux/X11" panzerdeathmatch3d-linux
+#/home/tim/code/godot/godot/bin/godot.x11.opt.tools.64.mono --export "Windows Desktop" panzerdeathmatch3d-windows
+
