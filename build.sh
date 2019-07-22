5deec79e706bb2d65367679dca2be8efbfa2a883
Tim Nibert <docz2a@gmail.com>
2019-07-22 23:06:10 +1000

    slight modification of build filename

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 # path to the godot executable
 GODOT=/home/tim/bin/Godot_v3.1.1-stable_mono_x11_64/Godot_v3.1.1-stable_mono_x11.64
 
-$GODOT --export "Linux/X11" panzerdeathmatch3d-linux
+$GODOT --export "Linux/X11" panzerdeathmatch3d-linux.x86_64
 $GODOT --export "Windows Desktop" panzerdeathmatch3d-windows.exe
 
